22d4d5731edabc71615d2bf26292f2bc36b559c3
Abraham Belay <127143063+abrahambelaygelaw@users.noreply.github.com>
2025-04-01 15:10:28 +0300

    update mock data and file row

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,17 @@
-import { type Config } from "drizzle-kit";
+// drizzle.config.ts
 
+import { defineConfig } from "drizzle-kit";
 import { env } from "~/env";
 
-export default {
-  schema: "./src/server/db/schema.ts",
-  dialect: "sqlite",
-  dbCredentials: {
-    url: env.DATABASE_URL,
-  },
-  tablesFilter: ["drive_*"],
-} satisfies Config;
+export default defineConfig({
+    dialect: "singlestore",
+    schema: "./src/server/db/schema.ts",
+    dbCredentials: {
+        host: env.SINGLESTORE_HOST,
+        user: env.SINGLESTORE_USER,
+        password:env.SINGLESTORE_PASS,
+        port: env.SINGLESTORE_PORT,
+        database: env.SINGLESTORE_DB_NAME,
+        ssl: {},
+    },
+});
