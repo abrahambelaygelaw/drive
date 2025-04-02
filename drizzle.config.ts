efb5e8cef3024ee03e669ae548ceff26bb8f8b2c
Abraham Belay <127143063+abrahambelaygelaw@users.noreply.github.com>
2025-04-03 00:09:23 +0300

    db model working\!

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -6,6 +6,7 @@ import { env } from "~/env";
 export default defineConfig({
     dialect: "singlestore",
     schema: "./src/server/db/schema.ts",
+    tablesFilter : ["drive_*"],
     dbCredentials: {
         host: env.SINGLESTORE_HOST,
         user: env.SINGLESTORE_USER,
@@ -14,4 +15,4 @@ export default defineConfig({
         database: env.SINGLESTORE_DB_NAME,
         ssl: {},
     },
-});
+});  
